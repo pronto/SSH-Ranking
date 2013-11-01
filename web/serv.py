4e549bf6eb0118ba6e554a21975b2e778f6479e0
pronto <pronto185@gmail.com>
2013-11-01 11:58:21 -0400

    redid the web-ui...again check out /alltime

--- web/serv.py
+++ web/serv.py
@@ -68,6 +68,7 @@ def week():
     userlist=[]
     datelist=[]
     deltime=[]
+    subhead="Last 7 days"
     lastweek=datetime.today()-timedelta(7)
     uniq_ips=Session.query(ips.ip,func.count(ips.ip).label('total')).group_by(ips.ip).\
             order_by('total DESC').filter(ips.dtime >= lastweek).limit(int(total_ip)).all()
@@ -83,7 +84,33 @@ def week():
             userlist.append((ip,user[0],user[1]))
     alldns=Session.query(rdns).all()
     newest=max(deltime)
-    return render_template('week.html',uniq_ips=uniq_ips,userlist=userlist,alldns=alldns,datelist=datelist,newest=newest)
+    return render_template('week.html',uniq_ips=uniq_ips,userlist=userlist,alldns=alldns,datelist=datelist,newest=newest,subhead=subhead)
+
+
+
+@app.route('/alltime')
+def alltime():
+    userlist=[]
+    datelist=[]
+    deltime=[]
+    subhead="All Time"
+    uniq_ips=Session.query(ips.ip,func.count(ips.ip).label('total')).group_by(ips.ip).\
+            order_by('total DESC').limit(int(total_ip)).all()
+    for ip in uniq_ips:
+        users = Session.query(ips.user,func.count(ips.user).label('total')).filter(ips.ip==str(ip[0])).group_by(ips.user).order_by('total DESC').limit(user_cnt).all()
+        date=Session.query(ips.dtime).filter(ips.ip==ip[0]).order_by(-ips.pk).limit(1).scalar()
+        date=datetime.strptime(str(date),'%Y-%m-%d %H:%M:%S')
+        date=date.strftime('%Y-%m-%d %H:%M:%S')
+        #ip.append(date)
+        datelist.append((ip[0],date))
+        deltime.append(date)
+        for user in users:
+            userlist.append((ip,user[0],user[1]))
+    alldns=Session.query(rdns).all()
+    newest=max(deltime)
+    return render_template('week.html',uniq_ips=uniq_ips,userlist=userlist,alldns=alldns,datelist=datelist,newest=newest, subhead="All Time")
+
+
 
 
 
