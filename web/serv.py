f658cc1bf8c35c7a34eefb6a387adc1985633f31
pronto <pronto185@gmail.com>
2013-11-01 08:45:33 -0400

    re-working the web-ui yet again, oh joy!

--- web/serv.py
+++ web/serv.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2.6
 from flask import Flask, render_template, send_from_directory, request
 import socket,os,sys
-from datetime import datetime
+from datetime import datetime,timedelta
+from datetime import date as ddate
 from ConfigParser import SafeConfigParser
 par = SafeConfigParser()
 #will be /etc/ssh-rank.ini or whereever you want it
@@ -38,6 +39,7 @@ app=Flask(__name__)
 def main():
     userlist=[]
     datelist=[]
+    deltime=[]
     uniq_ips=Session.query(ips.ip,func.count(ips.ip).label('total')).group_by(ips.ip).order_by('total DESC').limit(int(total_ip)).all()
     for ip in uniq_ips:
         users = Session.query(ips.user,func.count(ips.user).label('total')).filter(ips.ip==str(ip[0])).group_by(ips.user).order_by('total DESC').limit(user_cnt).all()
@@ -46,10 +48,43 @@ def main():
         date=date.strftime('%Y-%m-%d %H:%M:%S')
         #ip.append(date)
         datelist.append((ip[0],date))
+        deltime.append(date)
         for user in users:
             userlist.append((ip,user[0],user[1]))
     alldns=Session.query(rdns).all()
-    return render_template('main.html',uniq_ips=uniq_ips,userlist=userlist,alldns=alldns,datelist=datelist)
+    newest=max(deltime)
+    return render_template('main.html',uniq_ips=uniq_ips,userlist=userlist,alldns=alldns,datelist=datelist,newest=newest)
+
+
+
+@app.route('/new')
+def new():
+    #things
+    return render_template('new.html')
+
+
+@app.route('/week')
+def week():
+    userlist=[]
+    datelist=[]
+    deltime=[]
+    lastweek=datetime.today()-timedelta(7)
+    uniq_ips=Session.query(ips.ip,func.count(ips.ip).label('total')).group_by(ips.ip).\
+            order_by('total DESC').filter(ips.dtime >= lastweek).limit(int(total_ip)).all()
+    for ip in uniq_ips:
+        users = Session.query(ips.user,func.count(ips.user).label('total')).filter(ips.ip==str(ip[0])).group_by(ips.user).order_by('total DESC').limit(user_cnt).all()
+        date=Session.query(ips.dtime).filter(ips.ip==ip[0]).order_by(-ips.pk).limit(1).scalar()
+        date=datetime.strptime(str(date),'%Y-%m-%d %H:%M:%S')
+        date=date.strftime('%Y-%m-%d %H:%M:%S')
+        #ip.append(date)
+        datelist.append((ip[0],date))
+        deltime.append(date)
+        for user in users:
+            userlist.append((ip,user[0],user[1]))
+    alldns=Session.query(rdns).all()
+    newest=max(deltime)
+    return render_template('week.html',uniq_ips=uniq_ips,userlist=userlist,alldns=alldns,datelist=datelist,newest=newest)
+
 
 
 if __name__=='__main__':
